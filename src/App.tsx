c06caf1edf2f02e9b59ce2e778bbe663aef969ed
Caio Mazur <caiomazur1@gmail.com>
2024-02-07 15:37:46 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main className="flex w-screen h-screen items-center justify-center bg-gray-200">
+      <section className="bg-white flex flex-col items-center justify-center p-4 shadow-lg rounded-lg w-[500px] p-8">
+        <div className="flex flex-start flex-col w-full">
+          <h1 className="text-[52px] font-poppins font-bold">Template</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          purple: "hsl(259, 100%, 65%)",
+          "light-red": "hsl(0, 100%, 67%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          "off-white": "hsl(0, 0%, 94%)",
+          "light-grey": "hsl(0, 0%, 86%)",
+          "smokey-grey": "hsl(0, 1%, 44%)",
+          "off-black": "hsl(0, 0%, 8%)",
+        },
+      },
+      fontSize: {
+        "body-input": "32px",
+      },
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
